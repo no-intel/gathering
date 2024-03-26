860574d5e32a709b7d4c7775c7068050aa9f07ec
tkdduf212@gmail.com <tkdduf212@gmail.com>
2024-03-27 02:35:28 +0900

    reservation entity

---
+++ src/main/java/org/noint/gathering/entity/Reservation.java
@@ -0,0 +1,33 @@
+package org.noint.gathering.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class Reservation {
+    @Id @GeneratedValue
+    @Column(name = "reservation_id")
+    private Long id;
+
+    private String requestId;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "gathering_id")
+    private Gathering gathering;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "room_schedule_id")
+    private RoomSchedule roomSchedule;
+
+    public Reservation(String requestId, Gathering gathering, RoomSchedule roomSchedule) {
+        this.requestId = requestId;
+        this.gathering = gathering;
+        this.roomSchedule = roomSchedule;
+    }
+}
---
+++ src/test/java/org/noint/gathering/entity/ReservationTest.java
@@ -0,0 +1,52 @@
+package org.noint.gathering.entity;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.noint.gathering.entity.QReservation.reservation;
+
+@SpringBootTest
+@Transactional
+class ReservationTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    JPAQueryFactory queryFactory;
+
+    @Test
+    public void 예약_엔티티_테스트() throws Exception {
+        Room diamondRoom = new Room("다이아 룸", 10, BigDecimal.valueOf(20000));
+        em.persist(diamondRoom);
+        TimeSlot time = new TimeSlot("10:00 ~ 11:50");
+        em.persist(time);
+        RoomSchedule newSchedule = new RoomSchedule(diamondRoom, time);
+        em.persist(newSchedule);
+        Member user = new Member("a@b.c", "유저", "password");
+        em.persist(user);
+        Gathering newGathering = new Gathering("모각코", "각자 모여 코딩합시다", 4, user);
+        em.persist(newGathering);
+        Reservation newReservation = new Reservation("request_id", newGathering, newSchedule);
+        em.persist(newReservation);
+
+        em.flush();
+        em.clear();
+
+        List<Reservation> reservations = queryFactory
+                .selectFrom(reservation)
+                .fetch();
+        Member member = reservations.getFirst().getGathering().getMember();
+
+        assertThat(reservations.size()).isEqualTo(1);
+        assertThat(member.getName()).isEqualTo("유저");
+    }
+}
