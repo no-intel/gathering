a1829d7493728f6eadf73d231a7b6d22885ac109
tkdduf212@gmail.com <tkdduf212@gmail.com>
2024-03-28 08:49:01 +0900

    init sql user

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+insert into member (create_date,email,last_modified_date,money,name,password,status)
+values (NULL,'test1@b.c',NULL,0,'test1','$2a$10$3MDo3XjQiBifXBEReuPsduW5yAn283ERpolCjQM.ONVuoNUtZCyU6','REGISTERED'),
+       (NULL,'test2@b.c',NULL,0,'test2','$2a$10$3MDo3XjQiBifXBEReuPsduW5yAn283ERpolCjQM.ONVuoNUtZCyU6','REGISTERED'),
+       (NULL,'test3@b.c',NULL,0,'test3','$2a$10$3MDo3XjQiBifXBEReuPsduW5yAn283ERpolCjQM.ONVuoNUtZCyU6','REGISTERED'),
+       (NULL,'test4@b.c',NULL,0,'test4','$2a$10$3MDo3XjQiBifXBEReuPsduW5yAn283ERpolCjQM.ONVuoNUtZCyU6','REGISTERED'),
+       (NULL,'test5@b.c',NULL,0,'test5','$2a$10$3MDo3XjQiBifXBEReuPsduW5yAn283ERpolCjQM.ONVuoNUtZCyU6','REGISTERED'),
+       (NULL,'test6@b.c',NULL,0,'test6','$2a$10$3MDo3XjQiBifXBEReuPsduW5yAn283ERpolCjQM.ONVuoNUtZCyU6','REGISTERED'),
+       (NULL,'test7@b.c',NULL,0,'test7','$2a$10$3MDo3XjQiBifXBEReuPsduW5yAn283ERpolCjQM.ONVuoNUtZCyU6','REGISTERED'),
+       (NULL,'test8@b.c',NULL,0,'test8','$2a$10$3MDo3XjQiBifXBEReuPsduW5yAn283ERpolCjQM.ONVuoNUtZCyU6','REGISTERED'),
+       (NULL,'test9@b.c',NULL,0,'test9','$2a$10$3MDo3XjQiBifXBEReuPsduW5yAn283ERpolCjQM.ONVuoNUtZCyU6','REGISTERED'),
+       (NULL,'test10@b.c',NULL,0,'test10','$2a$10$3MDo3XjQiBifXBEReuPsduW5yAn283ERpolCjQM.ONVuoNUtZCyU6','REGISTERED')
+;
