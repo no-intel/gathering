abada1786491d0351de5ba5643a58bb025c53e92
tkdduf212@gmail.com <tkdduf212@gmail.com>
2024-04-01 17:40:15 +0900

    모임 예약 기능 api

--- src/main/java/org/noint/gathering/domain/reservation/controller/ReservationController.java
+++ src/main/java/org/noint/gathering/domain/reservation/controller/ReservationController.java
@@ -1,14 +1,15 @@
 package org.noint.gathering.domain.reservation.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.noint.gathering.domain.reservation.dto.request.ReserveGatheringReqDto;
 import org.noint.gathering.domain.reservation.dto.response.RoomScheduleResDto;
+import org.noint.gathering.domain.reservation.service.ReservationCommendService;
 import org.noint.gathering.domain.reservation.service.ReservationQueryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -20,9 +21,19 @@ public class ReservationController {
 
     private final ReservationQueryService reservationQueryService;
 
+    private final ReservationCommendService reservationCommendService;
+
     @GetMapping("/room-schedule/{date}")
     public ResponseEntity<List<RoomScheduleResDto>> getRoomSchedule(@PathVariable("date") LocalDate date) {
         log.info("날짜별 룸 스케줄 목록 조회 API");
         return new ResponseEntity<>(reservationQueryService.getDayRoomSchedule(date), HttpStatus.OK);
+    }
+
+    @PostMapping("/reservation")
+    public ResponseEntity<Void> reserveGathering(@RequestAttribute("memberId") Long memberId,
+                                                 @Valid @RequestBody ReserveGatheringReqDto request) {
+        log.info("모임 예약 API");
+        reservationCommendService.reserveGathering(memberId, request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
--- src/main/java/org/noint/gathering/domain/reservation/dto/request/ReserveGatheringReqDto.java
+++ src/main/java/org/noint/gathering/domain/reservation/dto/request/ReserveGatheringReqDto.java
@@ -1,9 +1,14 @@
 package org.noint.gathering.domain.reservation.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.util.List;
 
 public record ReserveGatheringReqDto(
+        @NotNull(message = "모임 값은 필 수 입니다.")
         Long gatheringId,
+
+        @NotNull(message = "최소 1개 이상의 스케줄이 필요 합니다.")
         List<Long> roomScheduleIds
 ) {
 }
