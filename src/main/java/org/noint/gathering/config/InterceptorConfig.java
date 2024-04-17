6b54036eb162e678a4b7b44d0e13abc4570a6ad7
tkdduf212@gmail.com <tkdduf212@gmail.com>
2024-04-18 03:50:04 +0900

    성능 test 환경 추가

--- src/main/java/org/noint/gathering/config/InterceptorConfig.java
+++ src/main/java/org/noint/gathering/config/InterceptorConfig.java
@@ -2,12 +2,14 @@ package org.noint.gathering.config;
 
 import lombok.RequiredArgsConstructor;
 import org.noint.gathering.config.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Component
 @RequiredArgsConstructor
+@Profile("!local-test")
 public class InterceptorConfig implements WebMvcConfigurer {
     private final LoginCheckInterceptor loginCheckInterceptor;
     @Override
